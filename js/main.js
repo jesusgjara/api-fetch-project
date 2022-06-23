8a07f205be81a42637ee5d649a1b65b061404003
Jesus G Jara <jesusgjara.1@gmail.com>
2022-06-22 23:22:21 -0400

    Adding new fetch, and style

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
+  const choice = document.querySelector('input').value.replaceAll(' ', '-').toLowerCase()
   const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        const potentialPet = new Poke (
-          data.name, data.height, data.weight, data.types, data.sprites.other["official-artwork"]["front_default"]
+        const potentialPet = new PokeInfo (
+          data.name, data.height, data.weight, data.types, data.sprites.other["official-artwork"]["front_default"], data.location_area_encounters
         )
         potentialPet.getTypes()
         potentialPet.isItHousePet()
+
         let decision = ""
         if(potentialPet.housepet) {
-          decision = 'This Pokemon is small enough, light enough, and safe enough to be a good pet!'
+          decision = `This Pokemon is small enough, light enough, and safe enough to be a good pet! You can find ${potentialPet.name} in the following location(s):`
+          potentialPet.encounterInfo()
         }else {
           decision = `This Pokemon would not be a good pet because ${potentialPet.reason.join(', ')}.`
+          document.getElementById('locations').innerText = ''
         }
         const height = `Height: ${potentialPet.height / 10}m`
         const weight = `Weight: ${Math.floor(potentialPet.weight / 10)}Kg`
@@ -80,3 +83,36 @@ class Poke {
   }
 }
 
+class PokeInfo extends Poke {
+  constructor (name, height, weight, types, image, location) {
+    super(name, height, weight, types, image)
+    this.locationURL = location
+    this.locationList = []
+    this.locationString = ''
+  }
+
+  encounterInfo() {
+    fetch(this.locationURL)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        for(const item of data) {
+          this.locationList.push(item.location_area.name)
+        }
+        let target = document.getElementById('locations')
+        target.innerText = this.locationCleanup()
+      })
+      .catch(err => {
+        console.log(`error ${err}`)
+    });
+  }
+
+  locationCleanup() {
+    const words = this.locationList.slice(0, 5).join(', ').replaceAll('-', ' ').split(' ')
+    for(let i=0; i<words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1)
+    }
+    return words.join(' ')
+  }
+
+}
